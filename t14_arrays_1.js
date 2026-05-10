d5524e1edc741a393e3e0f7f31f432068e9768da
24109jm <24109jm@hvhs.school.nz>
2026-05-11 09:55:53 +1200

    yfuygf

--- t14_arrays_1.js
+++ t14_arrays_1.js
@@ -9,9 +9,9 @@ function start() {
     const NAME_FIELD = document.getElementById("nameField");
     const BIRTHDAY_FIELD = document.getElementById("birthdayField");
     const MONEY_FIELD = document.getElementById("moneyField");
-    const CHOCOLATE_FIELD = document.getElementById("chocolateField");
-    const CHIPS_FIELD = document.getElementById("chipsField");
-    const DRINK_FIELD = document.getElementById("drinkField");
+    const CHOCOLATE_FIELD = document.getElementById("chocolate");
+    const CHIPS_FIELD = document.getElementById("chips");
+    const DRINK_FIELD = document.getElementById("drink");
     let name = NAME_FIELD.value;
     let birthday = BIRTHDAY_FIELD.value;
     let parts = birthday.split("/");
@@ -29,10 +29,11 @@ function start() {
     let hadBirthday =
         (day.getMonth() > monthBorn) ||
         (day.getMonth() === monthBorn && day.getDate() >= dayBorn);
+    let afford;
 
-    let chocolateArray = ["You HATE chocolate 😭", "You really don’t like chocolate.", "You’re not much of a chocolate person.", "You only like chocolate sometimes.", "You feel neutral about chocolate.", "You kinda like chocolate.", "You’re a chocolate fan.", "You really love chocolate!", "You’re obsessed with chocolate 😍", "You LIVE for chocolate 🍫✨"]
-    let chipsArray = ["You HATE chips 😭", "You really don’t like chips.", "You’re not much of a chips person.", "You only like chips sometimes.", "You feel neutral about chips.", "You kinda like chips.", "You’re a chips fan.", "You really love chips!", "You’re obsessed with chips 😍", "You LIVE for chips 🍟✨"]
-    let drinkArray = ["You HATE a drink 😭", "You really don’t like a drink.", "You’re not much of a drink person.", "You only like a drink sometimes.", "You feel neutral about a drink.", "You kinda like a drink.", "You’re a drink fan.", "You really love a drink!", "You’re obsessed with a drink 😍", "You LIVE for a drink 🥤✨"]
+    let chocolateArray = ["You HATE chocolate 😭", "You really don’t like chocolate.", "You’re not much of a chocolate person.", "You dislike chocolate slightly more than you like them.", "You feel neutral about chocolate.", "You kinda like chocolate.", "You’re a chocolate fan.", "You really love chocolate!", "You’re obsessed with chocolate 😍", "You LIVE for chocolate 🍫✨"]
+    let chipsArray = ["You HATE chips 😭", "You really don’t like chips.", "You’re not much of a chips person.", "You dislike chips slightly more than you like them.", "You feel neutral about chips.", "You kinda like chips.", "You’re a chips fan.", "You really love chips!", "You’re obsessed with chips 😍", "You LIVE for chips 🍟✨"]
+    let drinkArray = ["You HATE drinks 😭", "You really don’t like drinks.", "You’re not much of a drinks person.", "You dislike drinks slightly more than you like them.", "You feel neutral about drinks.", "You kinda like drinks.", "You’re a drinks fan.", "You really love drinks!", "You’re obsessed with drinks 😍", "You LIVE for drinks 🥤✨"]
 
     /****************************
     Main Code
@@ -55,11 +56,24 @@ function start() {
     function welcome() {
         OUTPUT.innerHTML += "<br>";
         OUTPUT.innerHTML += "<p>Welcome to the shop!</p>";
+        OUTPUT.innerHTML += "<br>";
     }
 
-    function displayProduct(_name, _price) {
-        let afford;
+    function calculateChange(_money, _price) {
+        let change = _money - _price
+        if (afford === "You can afford the ") {
+            OUTPUT.innerHTML += "<p>If you buy it, you will get $"+ change +" change.</p>"
+        } else {
+            OUTPUT.innerHTML += "<p></p>"
+        }
+    }
 
+    function like(array, field) {
+        OUTPUT.innerHTML += "<p>" + array[Number(field.value) - 1] + "</p>";
+        OUTPUT.innerHTML += "<br>"
+    }
+    
+    function displayProduct(_name, _price, array, field) {
         if (money >= _price) {
             afford = "You can afford the "
         } else {
@@ -68,15 +82,7 @@ function start() {
         OUTPUT.innerHTML += "<p>"+ _name +": $"+ _price +"</p>";
         OUTPUT.innerHTML += "<p> The "+ _name +" costs $"+ _price +". "+ afford + _name +" "+ getFace(money, _price)+ " </p>";
         calculateChange(money, _price)
-    }
-
-    function calculateChange(_money, _price) {
-        change = _money - _price
-        if (afford = "You can afford the ") {
-            OUTPUT.innerHTML += "If you buy it, you will get $"+ change +" change.</p>"
-        } else {
-            OUTPUT.innerHTML += "<p></p>"
-        }
+        like(array, field)
     }
 
     OUTPUT.innerHTML = "<p>Hi, "+ name +".</p>"
@@ -87,7 +93,7 @@ function start() {
     OUTPUT.innerHTML += "<p>If you spend half of your money, you will have $"+ (money / 2) +" "+ getFace(halfMoney, 100) +"</p>";
     OUTPUT.innerHTML += "<p>If you get $3 after spending half your money, you will have $"+ (money / 2 + 3) +" "+ getFace(halfMoney3, 100) +"</p>";
     welcome();
-    displayProduct("Chocolate bar", 4)
-    displayProduct("Chips", 3)
-    displayProduct("Drink", 2.50)
+    displayProduct("Chocolate bar", 4, chocolateArray, CHOCOLATE_FIELD)
+    displayProduct("Chips", 3, chipsArray, CHIPS_FIELD)
+    displayProduct("Drink", 2.50, drinkArray, DRINK_FIELD)
 }
